5644995039b55172a9c151ca34291fb17c746520
Ziqi He <heziqi4399@gmail.com>
2025-04-19 22:08:53 +0200

    feature: implement arraysDiff function for array comparison

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -7,3 +7,20 @@ export function withoutNulls(arr) {
 	// Use != instead of !==, this is how we can remove both null and undefined values.
 	return arr.filter((item) => item != null);
 }
+
+/**
+ * Diffing two arrays.
+ * To keep the code simple, the order difference of an items in the array is ignored.
+ * It might cause problem because if this diffing is used on array of style class,
+ * the order matters because a class comes later may overwrite the style of the
+ * classList comes earlier. This is the tradeoff made to keep the code simple.
+ * @param {Array} oldArray
+ * @param {Array} newArray
+ * @returns
+ */
+export function arraysDiff(oldArray, newArray) {
+	return {
+		added: newArray.filter((newItem) => !oldArray.includes(newItem)),
+		removed: oldArray.filter((oldItem) => !newArray.includes(oldItem)),
+	};
+}
