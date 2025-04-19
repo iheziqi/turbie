539a3d8ca42a0c85d5320a651936bf0e2c452728
Ziqi He <heziqi4399@gmail.com>
2025-04-19 22:08:43 +0200

    feature: implement objectsDiff function for object comparison

---
+++ packages/runtime/src/utils/objects.js
@@ -0,0 +1,18 @@
+/**
+ * Diffing two objects.
+ * @param {object} oldObj
+ * @param {object} newObj
+ * @returns {{added: any[], removed: any[], updated: any[]}}
+ */
+export function objectsDiff(oldObj, newObj) {
+	const oldKeys = Object.keys(oldObj);
+	const newKeys = Object.keys(newObj);
+
+	return {
+		added: newKeys.filter((key) => !(key in oldObj)),
+		removed: oldKeys.filter((key) => !(key in newObj)),
+		updated: newKeys.filter(
+			(key) => key in oldObj && oldObj[key] !== newObj[key]
+		),
+	};
+}
