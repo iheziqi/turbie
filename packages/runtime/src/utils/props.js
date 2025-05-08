420db99d7182103baac11c0bbbae827b880768d3
Ziqi He <heziqi4399@gmail.com>
2025-05-08 13:38:40 +0200

    feature: add extractPropsAndEvents function to handle component props and events

---
+++ packages/runtime/src/utils/props.js
@@ -0,0 +1,19 @@
+/**
+ * @typedef ExtractedPropsEvents
+ * @type {object}
+ * @property {Object.<string,Function>} events - The event listeners to add to the element.
+ * @property {Object.<string,any} props - The props to add to the element.
+ */
+
+/**
+ * Extracts the events and props of a component or element virtual node, ignoring
+ * the 'key' attribute.
+ *
+ * @param {import('../h').VNode} vdom
+ * @returns {ExtractedPropsEvents} the events and props of the component
+ */
+export function extractPropsAndEvents(vdom) {
+	const { on: events = {}, ...props } = vdom.props;
+
+	return { props, events };
+}
